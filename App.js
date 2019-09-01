82703f7fd4998ff81eece3bebead43a642abef98
Nate Ziarek <nziarek@directs.com>
2019-08-31 21:17:19 -0500

    Capturing screen image working

--- App.js
+++ App.js
@@ -1,10 +1,54 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Button,
+  LayoutAnimation
+} from "react-native";
+
+import { captureRef as takeSnapshotAsync } from "react-native-view-shot";
 
 export default function App() {
+  const [image, setImage] = useState(null);
+  const [color, setColor] = useState(Math.round(Math.random() * 1000));
+
+  generatePDF = async () => {
+    let snapshot = await takeSnapshotAsync(this.myView);
+    if (snapshot.startsWith("/")) {
+      snapshot = "file://" + snapshot;
+    }
+    LayoutAnimation.configureNext(LayoutAnimation.Presets.spring);
+    setImage(snapshot);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View
+        ref={component => (this.myView = component)}
+        style={{ padding: 10, backgroundColor: `#${color}` }}
+      >
+        <Text style={{ color: "white" }}>
+          Open up App.js to start working on your app!
+        </Text>
+      </View>
+      <Button title="Snappy snap" onPress={this.generatePDF} />
+      <Button
+        title="Change Color"
+        onPress={() => {
+          LayoutAnimation.configureNext(LayoutAnimation.Presets.spring);
+          setColor(Math.round(Math.random() * 1000));
+        }}
+      />
+      {image && (
+        <Image
+          style={{ width: 300, height: 200, resizeMode: "contain" }}
+          source={{
+            uri: image
+          }}
+        />
+      )}
     </View>
   );
 }
@@ -12,8 +56,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
